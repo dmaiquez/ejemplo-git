95a72b019870691fad19cfc0d9c8879d46fc89cd
MATUTINO <MATUTINO@DESKTOP-FFBVPJ2>
2023-12-20 10:35:04 -0500

    001

--- src/com/distribuida/entities/pais.java
+++ src/com/distribuida/entities/pais.java
@@ -6,5 +6,43 @@ public class pais {
 		// TODO Auto-generated method stub
 
 	}
-
+	private int idpais;
+	private String nompais;
+	private String descripais;
+	private String idioma;
+	private String codigopais;
+	
+	public pais (){ }
+	
+	
+	public int getIdpais() {
+		return idpais;
+	}
+	public void setIdpais(int idpais) {
+		this.idpais = idpais;
+	}
+	public String getNompais() {
+		return nompais;
+	}
+	public void setNompais(String nompais) {
+		this.nompais = nompais;
+	}
+	public String getDescripais() {
+		return descripais;
+	}
+	public void setDescripais(String descripais) {
+		this.descripais = descripais;
+	}
+	public String getIdioma() {
+		return idioma;
+	}
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+	public String getCodigopais() {
+		return codigopais;
+	}
+	public void setCodigopais(String codigopais) {
+		this.codigopais = codigopais;
+	}
 }
